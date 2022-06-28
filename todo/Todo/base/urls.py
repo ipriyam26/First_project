8a7f29fa3989af22429e159c70b1c192efe11fc4
ipriyam26 <ipriyam26@gmail.com>
2022-06-28 13:49:28 +0530

    login

--- todo/Todo/base/urls.py
+++ todo/Todo/base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import *
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page='login'), name="logout"),
     path('', TaskList.as_view(), name='tasks'),
     path('create/', TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task-detail'),
-    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update')
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/',TaskDelete.as_view(),name='task-delete'),
 ]
--- todo/Todo/base/views.py
+++ todo/Todo/base/views.py
@@ -1,32 +1,65 @@
-from typing import Optional, Type, Union
+from typing import Any, Dict, Optional, Type, Union
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
+
 # Create your views here.
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name: str='base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user: bool = True
+    
+    def get_success_url(self) -> str:
+        
+        return reverse_lazy('tasks')
+
+
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     template_name = 'base/task_list.html'
     context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['tasks']= context['tasks'].filter(user=self.request.user)
+        context['count']= context['tasks'].filter(complete=False).count()
+        return context
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin,DetailView):
     model= Task
     context_object_name: Optional[str] = 'task'
     template_name: str = 'base/task.html'
 
-class TaskCreate(CreateView):
+
+class TaskCreate(LoginRequiredMixin,CreateView):
     model: Type[Task] = Task
     fields = '__all__'
     success_url: Optional[str] = reverse_lazy('tasks')
+        
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form=form)
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model: Type[Task]= Task
     fields = '__all__'
     success_url: Optional[str] = reverse_lazy('tasks')
+    
+class TaskDelete(LoginRequiredMixin,DeleteView):
+    model: Type[Task]  = Task
+    context_object_name: Optional[str] = 'task'
+    success_url: Optional[str] = reverse_lazy('tasks')
+
+
+
+
     
     
  
